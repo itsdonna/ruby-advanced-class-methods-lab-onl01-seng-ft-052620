5b12b9e7e667db84834544e11cfaf0e4db669fbf
Donna Torres <todonnatorres@icloud.com>
2020-06-08 23:02:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,27 +1,33 @@
 class Song
+  
   attr_accessor :name, :artist_name
     @@all = []
     def self.all
       @@all 
     end
+    
   def self.create
     song = self.new 
     song.save
     song
   end
+  
   def save
     self.class.all << self
   end
+  
   def self.new_by_name(name)
     song = self.new
     song.name = name
     song
   end
+  
   def self.create_by_name(name)
     son = self.create 
     son.name = name
     son
   end
+  
   def self.find_by_name(name)
     @@all.each do |song| 
     if song.name == name  
@@ -34,9 +40,11 @@ class Song
   def self.find_or_create_by_name(name)
      self.find_by_name(name) || self.create_by_name(name)
   end
+  
    def self.alphabetical
      @@all.sort_by {|x| x.name}
    end
+   
    def self.new_from_filename(name)
         x = name.gsub(".mp3", "").split(/\-/)
         song = self.create_by_name(x[1].strip)
