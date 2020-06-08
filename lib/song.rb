545e5117ec758eb72642d4287eeb6728be2383e7
Donna Torres <todonnatorres@icloud.com>
2020-06-08 22:45:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,8 +31,10 @@ class Song
     nil
   end
   def self.find_or_create_by_name(name)
-    self.find_by_name(name)
-    self.create_by_name(name)
+    if self.find_by_name(name)
+     elsif self.create_by_name(name)
+   else self.create_by_name
+ end
   end
 end
 
