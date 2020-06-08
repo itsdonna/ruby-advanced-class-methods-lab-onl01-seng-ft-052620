cb96d1533e7134052405f2b4df1b6db398969f88
Donna Torres <todonnatorres@icloud.com>
2020-06-08 23:10:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,7 +55,7 @@ class Song
     def self.create_from_filename(name)
       self.new_from_filename(name)
     end
-    def self.destroy_all
+    def self.destroy_all(test)
       self.clear
   end
 
